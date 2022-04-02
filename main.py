33e762ac20d626ae342403d06029b4458c570806
Ghounder <hounderlagger@gmail.com>
2022-04-02 03:18:08 -0500

    reques_response_body

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
+
+from fastapi import FastAPI,Body
 
 app = FastAPI()
+
+#Models
+
+class person(BaseModel):
+    first_name : str
+    last_name : str
+    age : int
+    hair_color : Optional[str] = None
+    is_married : Optional[bool] = None
 
 @app.get("/")
 def home():
@@ -9,3 +21,9 @@ def home():
 @app.get("/platzi")
 def platzi():
     return { "Hello" : "World"}
+
+#request and response body
+
+@app.post("/person/new")
+def create_person(person : person = Body(...)):
+    return person
